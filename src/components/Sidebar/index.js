b9cdb11d1a730a9d719b81c56ea4d10c949983db
cesar-gz <cesargutierrezceja@gmail.com>
2022-08-29 20:53:02 -0700

    added assests.

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,13 +1,15 @@
 import { Link } from 'react-router-dom'
 import './index.scss'
-import LogoS from '../../assets/images/logo-s.png'
+//import LogoS from '../../assets/images/logo-s.png'
+import LogoS from '../../assets/images/C1.jpg'
+//***********************I left off trying to find a giant C and Gutierrez for nav bar, 12:45 timestamp */
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 
 const Sidebar = () => (
     <div className='nav-bar'>
         <Link className='logo' to='/'>
             <img src={LogoS} alt="logo" />
-            <img src={LogoSubtitle} alt="slobodan" />
+            <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
         </Link>
     </div>
 )
