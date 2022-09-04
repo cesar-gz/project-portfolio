28af0b3ce9b75fa57a89b1050b2f2ea6e63a8d26
cesar-gz <cesargutierrezceja@gmail.com>
2022-09-04 10:00:38 -0700

    Pushing progress.

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,9 +1,10 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
-//import LogoS from '../../assets/images/logo-s.png'
-import LogoS from '../../assets/images/C1.jpg'
-//***********************I left off trying to find a giant C and Gutierrez for nav bar, 12:45 timestamp */
-import LogoSubtitle from '../../assets/images/logo_sub.png'
+import LogoS from '../../assets/images/logo-c.png'
+import LogoSubtitle from '../../assets/images/logo-sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin, faSkype } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
     <div className='nav-bar'>
@@ -11,6 +12,60 @@ const Sidebar = () => (
             <img src={LogoS} alt="logo" />
             <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
         </Link>
+        <nav>
+            <NavLink 
+                exact="true" 
+                activeclassname="active" 
+                to="/"
+            > 
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            </NavLink>
+            <NavLink 
+                exact="true" 
+                activeclassname="active" 
+                className="about-link" 
+                to="/about"
+            > 
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+            </NavLink>
+            <NavLink 
+                exact="true" 
+                activeclassname="active" 
+                className="contact-link" 
+                to="/contact"
+            > 
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+            </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a target = "_blank"
+                   rel="noreferrer" 
+                   href="https://www.linkedin.com/in/cesar-gutierrez-c/"
+                >
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                </a>
+            </li>
+
+            <li>
+                <a target = "_blank"
+                   rel="noreferrer" 
+                   href="https://github.com/cesar-gz"
+                >
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
+
+            <li>
+                <a target = "_blank"
+                   rel="noreferrer" 
+                   href="skype:live:.cid.9e1cfdd2ab6bfd6c"
+                >
+                    <FontAwesomeIcon icon={faSkype} color="#4d4d4e" />
+                </a>
+            </li>
+
+        </ul>
     </div>
 )
 
