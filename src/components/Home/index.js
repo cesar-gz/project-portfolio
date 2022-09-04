9a26cd361ad02c4fa4aaf322f19537b2b6ba225b
cesar-gz <cesargutierrezceja@gmail.com>
2022-09-04 15:32:30 -0700

    finished home page so far.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,9 @@
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import LogoTitle from '../../assets/images/logo-c2.png';
-import AnimatedLetters from '../AnimatedLetters';
-import './index.scss';
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import LogoTitle from '../../assets/images/logo-c2.png'
+import AnimatedLetters from '../AnimatedLetters'
+import Logo from './Logo'
+import './index.scss'
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -39,7 +40,7 @@ const Home = () => {
                 <h2>Learning to make secure and reliable software</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
-
+            <Logo />
         </div>
     )
 }
