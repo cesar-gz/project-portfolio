52a0ffb133e9344769362f3043a93945ab623228
cesar-gz <cesargutierrezceja@gmail.com>
2022-09-04 13:49:09 -0700

    added animation effects to home page title and subtitle text.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,25 +1,47 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-c2.png';
+import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['e', 's', 'a', 'r']
+    const jobArray = ['a',' ', 'c', 'o', 'm', 'p', 'u', 't','e','r',' ', 's','c','i','e','n','c','e',' ','s',
+                      't','u','d','e','n','t', '.']
 
+    // note to self: I had a problem at 40:06, fix was to remove return from setTimeout and add 
+    // the return to setLetterClass
+    useEffect(() => {
+        setTimeout(() => {
+            return setLetterClass('text-animate-hover')
+        }, 4000)
+    }, [])
 
     return(
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br /> I'm
-                <img src={LogoTitle} alt="developer" />
-                esar,
+                <h1>
+                <span className={letterClass}>H</span>
+                <span className={`${letterClass} _12`}>i</span> 
                 <br />
-                a computer science student
+                <span className={`${letterClass} _13`}>I</span> 
+                <span className={`${letterClass} _14`}>'m</span> 
+                <img src={LogoTitle} alt="developer" />
+                <AnimatedLetters letterClass={letterClass} 
+                strArray={nameArray}
+                idx={15}/>
+                <br />
+                <AnimatedLetters letterClass={letterClass} 
+                strArray={jobArray}
+                idx={19}/>
                 </h1>
                 <h2>Learning to make secure and reliable software</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
 
         </div>
-    );
+    )
 }
 
-export default Home;
+export default Home
