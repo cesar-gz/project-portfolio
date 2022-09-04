9f1dfe9c3c322d5ad354f35f35aaa7c6150e512c
cesar-gz <cesargutierrezceja@gmail.com>
2022-09-04 11:49:47 -0700

    pushing progress.

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,11 +1,27 @@
+import { Outlet } from 'react-router-dom'
 import Sidebar from '../Sidebar'
 import './index.scss';
 
+// In the <span className='bottom-tag-html' > x </span>
+// x is the actual text you want to edit later so that the layout makes sense
+
 const Layout = () => {
     return (
-    <>
-    <Sidebar />
-    </>
+    <div className="App">
+        <Sidebar />
+        <div className='page'>
+            <span className='tags top-tags'>&lt;body&gt;</span>
+
+            <Outlet />
+
+            <span className='tags bottom-tags'>
+            &lt;body&gt;
+            <br />
+            <span className='bottom-tag-html'>&lt;html&gt;</span>
+
+            </span>
+        </div>
+    </div>
     )
 }
 
