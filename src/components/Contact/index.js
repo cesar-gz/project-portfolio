183be88d6cc6d0cb0d7a2a8a67a61f71b889069b
cesar-gz <cesargutierrezceja@gmail.com>
2022-09-04 19:32:14 -0700

    Added email functionality and google map location.

---
+++ src/components/Contact/index.js
@@ -0,0 +1,115 @@
+import { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
+import './index.scss'
+import { useRef } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
+import emailjs from '@emailjs/browser'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
+const Contact = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const form = useRef()
+
+    useEffect(() => {
+        setTimeout(() => {
+            return setLetterClass('text-animate-hover')
+        }, 3000)
+    }, [])
+
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        emailjs
+            .sendForm(
+                'service_3bjfqaa',
+                'template_ky0v96c',
+                form.current,
+                'y04LRN06MzREufY6-'
+            )
+            .then(
+                () => {
+                    alert('Message successfully sent!')
+                    window.location.reload(false)
+                },
+                () => {
+                    alert('Failed to send the message, please try again.')
+                }
+            )
+    }
+
+    return(
+        <>
+            <div className='container contact-page'>
+                <div className='text-zone'>
+                <h1>
+                    <AnimatedLetters 
+                    letterClass={letterClass}
+                    strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+                    idx={15}
+                    />
+                </h1>
+                <p>
+                    I am interested in internship opportunities, especially ones where I can be hired after
+                    graduating college. However, if you have other requests or questions, feel free to contact
+                    me using the form below.
+                </p>
+                <div className='contact-form'>
+                    <form ref={form} onSubmit={sendEmail}>
+                        <ul>
+                            <li className='half'>
+                                <input type="text" name="name" placeholder="Name" required />
+                            </li>
+                            <li className='half'>
+                                <input 
+                                type="email" 
+                                name="email" 
+                                placeholder="Email" 
+                                required 
+                                />
+                            </li>
+                            <li>
+                                <input 
+                                placeholder="Subject" 
+                                type="text" 
+                                name="subject" 
+                                required 
+                                />
+                            </li>
+                            <li>
+                                <textarea 
+                                placeholder="Message"
+                                name="message"
+                                required>
+                                </textarea>
+                            </li>
+                            <li>
+                                <input type="submit" className='flat-button' value="SEND" />
+                            </li>
+                        </ul>
+                    </form>
+                </div>
+                </div>
+            </div>
+            <div className="info-map">
+                Cesar Gutierrez,
+                <br />
+                United States,
+                <br />
+                800 N State College Blvd,<br />
+                Fullerton, CA 92831 <br />
+                <span>cesar7720g@gmail.com</span>
+            </div>
+            <div className='map-wrap'>
+                <MapContainer center={[33.8750, -117.8845]} zoom={13}>
+                    <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                    <Marker position={[33.8750, -117.8845]}>
+                        <Popup>I study here, and I like drinking monster energy drinks :) </Popup>
+                    </Marker>
+                </MapContainer>
+            </div>
+            <Loader type="pacman" />
+        </>
+    )
+}
+
+export default Contact
