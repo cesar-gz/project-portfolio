4d032760c47e13ddcead212815f3cf7727b8e410
cesar-gz <cesargutierrezceja@gmail.com>
2022-09-05 09:36:46 -0700

    finished portfolio template.

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -31,6 +31,14 @@ const Portfolio = () => {
                                 src={port.cover}
                                 className="portfolio-image"
                                 alt={"portfolio"} />
+                                <div className="content">
+                                    <p className="title">{port.title}</p>
+                                    <h4 className="description">{port.description}</h4>
+                                    <button
+                                        className="btn"
+                                        onClick={() => window.open(port.url)}
+                                    >View</button>
+                                </div>
                             </div>
                         )
                     })
